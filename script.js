a28d734c8a8c9d0bfeaf086059d6ba57464fe3d3
Ikenna Richard <oguejioforrichard@gmail.com>
2022-12-31 11:41:39 -1100

    📄 Add script file

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const menuIcon = document.querySelector('.menu_icon');
+
+const cover = document.querySelector('.cover');
+
+const extraLayer = document.querySelector('.extra_layer');
+
+const menuLinks = [...document.querySelectorAll('.menu_links > a')];
+
+const arrow = [...document.querySelectorAll('.arrow')];
+
+const navList = [...document.querySelectorAll('.cover nav ul')];
+
+/*slide menu button */
+
+
+menuIcon.addEventListener('click', menuSlide);
+
+menuLinks.forEach((item, i) => item.addEventListener('click', () => {
+
+    arrow[i].classList.toggle('show_nav');
+    navList[i].classList.toggle('show');
+
+
+}));
+
+
+function menuSlide() {
+    this.classList.toggle('change');
+    cover.classList.toggle('show_cover');
+    extraLayer.classList.toggle('show_cover');
+}
+
+
+
